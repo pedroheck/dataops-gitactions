7863cfa41eea736589d580f16b3d23e56bdaaba3
pedroheck <pedroscheck@hotmail.com>
2023-05-03 21:50:45 -0300

    remove

--- python_names_storage.py
+++ python_names_storage.py
@@ -3,6 +3,7 @@ from google.cloud import storage
 from google.oauth2 import service_account
 import requests
 
+
 headers = {
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 }
@@ -21,7 +22,7 @@ credentials_dict = {
 }
 
 try:
-  
+
   """Uploads a file to the bucket."""
   credentials = service_account.Credentials.from_service_account_info(credentials_dict)
   storage_client = storage.Client(credentials)
